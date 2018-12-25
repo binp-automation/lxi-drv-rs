422b01b242309f4fbc92e49af32bc9a6b774f308
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-12-25 10:28:47 +0700

    messages for tcp proxy

--- src/proto/tcp.rs
+++ src/proto/tcp.rs
@@ -2,16 +2,63 @@ use std::collections::{VecDeque};
 
 use ::channel::{Sender};
 use ::proxy::{self, Control, Eid};
-use ::wrapper::{self, UserProxy, UserHandle};
+use ::wrapper::{self as w, UserProxy, UserHandle};
 
 
 pub enum Tx {
-
+    // Base
+    Close,
 }
+
+impl From<w::Tx> for Tx {
+    fn from(other: w::Tx) -> Tx {
+        match other {
+            w::Tx::Close => Tx::Close,
+        }
+    } 
+}
+
+impl Into<Result<w::Tx, Tx>> for Tx {
+    fn into(self) -> Result<w::Tx, Tx> {
+        match self {
+            Tx::Close => Ok(w::Tx::Close),
+            other => Err(other),
+        }
+    }
+}
+
+impl w::TxExt for Tx {}
 
 pub enum Rx {
-
+    // Base
+    Attached,
+    Detached,
+    Closed,
 }
+
+impl From<w::Rx> for Rx {
+    fn from(other: w::Rx) -> Rx {
+        match other {
+            w::Rx::Attached => Rx::Attached,
+            w::Rx::Detached => Rx::Detached,
+            w::Rx::Closed => Rx::Closed,
+        }
+    } 
+}
+
+impl Into<Result<w::Rx, Rx>> for Rx {
+    fn into(self) -> Result<w::Rx, Rx> {
+        match self {
+            Rx::Attached => Ok(w::Rx::Attached),
+            Rx::Detached => Ok(w::Rx::Detached),
+            Rx::Closed => Ok(w::Rx::Closed),
+            other => Err(other),
+        }
+    }
+}
+
+impl w::RxExt for Rx {}
+
 
 pub struct Proxy {
     tx: Option<Sender<Rx>>,
@@ -60,12 +107,18 @@ impl UserHandle<Tx, Rx> for Handle {
     fn set_send_channel(&mut self, tx: Sender<Tx>) {
         self.tx = Some(tx);
     }
-    fn process_recv_channel(&mut self, msg: Rx) -> ::Result<()> {
-        self.msgs.push_back(msg);
+    fn process_recv_channel(&mut self, _msg: Rx) -> ::Result<()> {
         Ok(())
     }
 }
 
-pub fn create() -> ::Result<(ProxyWrapper<Proxy, Tx, Rx>, HandleWrapper<Handle, Tx, Rx>)> {
-    proxy_handle::create(Proxy::new(), Handle::new())
+pub fn create() -> ::Result<(w::Proxy<Proxy, Tx, Rx>, w::Handle<Handle, Tx, Rx>)> {
+    w::create(Proxy::new(), Handle::new())
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
 }
